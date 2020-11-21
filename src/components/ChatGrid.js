427935a7407a1b17e1c07c2465acef32a2e04f95
wPresti <wpresti@gmail.com>
2020-11-21 15:31:28 -0800

    test

--- src/components/ChatGrid.js
+++ src/components/ChatGrid.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class ChatGrid extends Component{
 
     render(){
-        return <h1>hello world</h1>
+        return <div className="grid-container">
+                <div className="item1">Header</div>
+                <div className="item2">Menu</div>
+                <div className="item3">Main</div>  
+                <div className="item4">Right</div>
+                <div className="item5">Footer</div>
+        </div>
     }
 }
 export default ChatGrid
