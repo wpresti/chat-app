33e0f4282bde1a717d827f3b3322d98f1f1ffd54
wPresti <wpresti@gmail.com>
2020-11-21 15:25:53 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ChatGrid from './components/ChatGrid' 
+
+
+function App() {
+  return (
+    <div className="App">
+      <ChatGrid></ChatGrid>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ChatGrid.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class ChatGrid extends Component{
+
+    render(){
+        return <h1>hello world</h1>
+    }
+}
+export default ChatGrid
