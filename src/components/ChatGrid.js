0b7ad4b47ac11ada2ca2caa2004345df4d1b811c
wPresti <wpresti@gmail.com>
2020-11-21 16:55:41 -0800

    grid is broken :(

--- src/components/ChatGrid.js
+++ src/components/ChatGrid.js
@@ -1,15 +1,39 @@
 import React, { Component } from 'react';
 
-class ChatGrid extends Component{
+export class ChatGrid extends Component{
 
     render(){
-        return <div className="grid-container">
-                <div className="item1">Header</div>
-                <div className="item2">Menu</div>
-                <div className="item3">Main</div>  
-                <div className="item4">Right</div>
-                <div className="item5">Footer</div>
+        return <div className="grid-container" style={gridContainer}>
+                <div className="header" style={Object.assign({}, header, gridElement)}>Header</div>
+                <div className="menu" style={Object.assign({}, menu, gridElement)}>Menu</div>
+                <div className="main" style={Object.assign({}, main, gridElement)}>Main</div>  
+                <div className="right" style={Object.assign({}, right, gridElement)}>Right</div>
+                <div className="footer" style={Object.assign({}, footer, gridElement)}>Footer</div>
         </div>
     }
 }
 export default ChatGrid
+
+const header = {girdArea: "header"}
+const menu = {gridArea: "menu"}
+const main = {gridArea: "main"}
+const right = {gridArea: "right"}
+const footer = {gridArea: "footer"}
+
+const gridContainer = {
+    display: "grid",
+    gridTemplateAreas: `'header header header header header header'
+    'menu main main main right right'
+    'menu main main main right right'
+    'menu footer footer footer footer footer'`,
+    gridGap: "10px",
+    backgroundColor: "#2196F3",
+    padding: "10px" 
+}
+
+const gridElement = {
+    backgroundColor: "rgba(255,255,255,0.8)",
+    textAlign: "center",
+    padding: "20px",
+    fontSize: "30px"
+}
